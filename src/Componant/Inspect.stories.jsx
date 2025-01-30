2d527b1afe4fbbd049c625eda16364d708e9f2a3
altayyabsikandar <altayyabsikandar@gmail.com>
2025-01-30 11:07:05 +0530

    first commit

---
+++ src/Componant/Inspect.stories.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import InspectC from "./InspectC";
+export default{
+    title:"Componant/InspectCar",
+    Componant:InspectC,
+};
+
+export const Default=()=><InspectC/>
---
+++ src/Componant/InspectC.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./stories.css"
+const InspectC = () => {
+  return (
+    <div className="p">
+         <div className="head">
+             <h2 className="text">
+               Download expert inspection report to check Maruti   Alto K10's condition
+             </h2>
+         </div>
+         <div className="new">
+           <div className="a">
+             <ul className="space">
+             <li>
+               < strong>Check for Imperfections & Repairs</strong>
+             <br />
+               Be assured before making a purchase
+             </li>
+             <br>
+             </br>
+             <li>
+             <strong>Engine & 6 other components' details</strong>
+             <br />
+             Exterior, interior, electricals, tyres +2 more
+             </li>
+             <br>
+             </br>
+             <li>
+             <strong>Over 220+ Points of Inspection</strong>
+             <br />
+             Inspected by our expert engineers
+             </li>
+             </ul>
+             </div>
+   
+             <div className="b">
+               <img src="https://stimg.cardekho.com/pwa/img/ucr/inspection/inpection-illustration.svg" alt="" />
+             </div>
+           </div>
+           <br>
+             </br>
+             <br>
+             </br>
+             <br>
+             </br>
+             <br>
+             </br>
+         <button className="w"> 
+           Download Inspection Report
+         </button>
+       </div>
+  )
+}
+
+export default InspectC
