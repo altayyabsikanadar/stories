245916e4b13e6213cde859c6e0931b1f95b5dbf3
altayyabsikandar <altayyabsikandar@gmail.com>
2025-02-14 16:13:13 +0530

    trust commit

--- src/Justdeal/Justdeal.jsx
+++ src/Justdeal/Justdeal.jsx
@@ -11,10 +11,10 @@ export const Justdeal=()=>{
             name:'Altayyab'
         },
         {
-            name:'altayyab'
+            name:'Altayyab'
         },
         {
-            // name:'altayyab'
+            // name:'Altayyab'
         },
     ])
 
